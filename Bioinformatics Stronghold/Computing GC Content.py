4a1a3752e7c0d268bbe7f4d81c13676a69e87214
Jasmin Lin <jazzhands2112@gmail.com>
2024-11-13 12:55:47 -0500

    Update Computing GC Content.py
    Uploaded the correct code.

--- Bioinformatics Stronghold/Computing GC Content.py
+++ Bioinformatics Stronghold/Computing GC Content.py
@@ -1,4 +1,48 @@
-d = open('example.txt', 'r')
+#Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+#Return: The ID of the string having the highest GC-content, followed by the GC-content of that string
 
-seperate = []
+import os
 
+
+def seperate(fastatext):
+    gc = fastatext.count('G') + fastatext.count('C')
+    percentGC = gc / len(fastatext) * 100
+    return percentGC
+
+
+if __name__ == '__main__':
+    data = open("C:/Users/horse/Downloads/SBU/PythonTings/example.txt",'r')
+    readata = data.read()
+    
+    datasett = readata.split('>')
+    #print(datasett)
+
+    middleman = {}
+
+    for sections in datasett:
+        line = sections.split('\n') #split the split file lol
+        identification = line[0] #first line of each split string
+        sequence = ''.join(line[1:])
+        middleman[identification] = sequence
+
+    #print(middleman)
+
+    edited = list(middleman.items())
+    if edited:
+        edited.pop(0)
+    
+    middleman = dict(edited)
+
+    #print(middleman)
+
+    highscore = 0
+    highscoreID = None
+
+    for identification, sequence in middleman.items():
+        gcpercentage = seperate(sequence)
+
+        if gcpercentage > highscore:
+            highscore = gcpercentage
+            highscoreID = identification
+
+    print(str(highscoreID) + ' ' + str(highscore))
