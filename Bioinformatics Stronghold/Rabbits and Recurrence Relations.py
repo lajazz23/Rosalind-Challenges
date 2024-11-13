44099669b5c9afd8cc6193bd74d40bc8fe12d87c
Jasmin Lin <jazzhands2112@gmail.com>
2024-11-13 12:52:33 -0500

    Update Rabbits and Recurrence Relations.py
    Added notes.

--- Bioinformatics Stronghold/Rabbits and Recurrence Relations.py
+++ Bioinformatics Stronghold/Rabbits and Recurrence Relations.py
@@ -1,5 +1,8 @@
-n = 31
-k = 5
+# Given: Positive integers n≤40 and k≤5.
+# Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
+
+n = 31 # given by Rosalind
+k = 5 # given by Rosalind
 
 def wabbits (n,k):
     if n == 0:
