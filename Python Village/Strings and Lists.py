dbdee9e83e0ae9a81be25316ae22842073b68fab
Jasmin Lin <jazzhands2112@gmail.com>
2024-11-13 12:38:22 -0500

    Updated Strings and Lists.py
    Added instructions.

--- Python Village/Strings and Lists.py
+++ Python Village/Strings and Lists.py
@@ -1,3 +1,7 @@
+# Given: A string s of length at most 200 letters and four integers a, b, c and d.
+# Return: The slice of this string from indices a through b and c through d (with space in between), inclusively. In other words, we should include elements s[b] and s[d] in our slice.
+
+# the code for the above prompt can be found below. This section was mainly me following the Rosalind tutorials.
 tea_party = ['March Hare', 'Hatter', 'Dormouse', 'Alice']
 tea_party[1] = 'Cheshire Cat'
 tea_party.append('Jabberwocky') #to add to end of the list
@@ -14,7 +18,7 @@ c = b[0] + a[2:]
 print c
 
 #challenge:
-stist = 'bxc5fCZ78E3sTsT3oNNHyz5yJ3Ni6jIUCpkDyd3VtvAhEQPtjDQgPhrynomerusVWL6BZpJUyNTJ4IN4eAXDnPMaHbShparadoxusqGZoDoTu8C6YO2hICZW12RLdLk6w0hfjyWJdIu1jZnXPUbKrQQAaJFg3HNBnYBLNq.'
+stist = 'bxc5fCZ78E3sTsT3oNNHyz5yJ3Ni6jIUCpkDyd3VtvAhEQPtjDQgPhrynomerusVWL6BZpJUyNTJ4IN4eAXDnPMaHbShparadoxusqGZoDoTu8C6YO2hICZW12RLdLk6w0hfjyWJdIu1jZnXPUbKrQQAaJFg3HNBnYBLNq.' #this is the string given by the Rosalind Challenge, manually inputted
 a = 52
 b = 62
 c = 92
