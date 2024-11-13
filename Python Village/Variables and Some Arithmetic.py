8e088a857c2cc716ff669fca2dcec231902a0ecc
Jasmin Lin <jazzhands2112@gmail.com>
2024-11-13 12:39:25 -0500

    Updated Variables and Some Arithmetic.py
    Added instructions.

--- Python Village/Variables and Some Arithmetic.py
+++ Python Village/Variables and Some Arithmetic.py
@@ -1,3 +1,6 @@
+# Given: Two positive integers a and b, each less than 1000.
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
 a = 937
 b = 810
 c = (a*a) + (b*b)
