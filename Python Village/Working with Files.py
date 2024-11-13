725f5b470d7af7d76260187fae9e7af5e598619f
Jasmin Lin <jazzhands2112@gmail.com>
2024-11-13 12:41:08 -0500

    Updated Working with Files.py
    Added instructions

--- Python Village/Working with Files.py
+++ Python Village/Working with Files.py
@@ -1,24 +1,28 @@
-#f = open('input.txt', 'r')
-#n = 10000
-#f.read(n)
+# Given: A file containing at most 1000 lines.
+# Return: A file containing all the even-numbered lines from the original file. Assume 1-based numbering of lines.
 
-#f.readline()
+# the code for the above prompt can be found below. This section was mainly me following the Rosalind tutorials.
+f = open('input.txt', 'r')
+n = 10000
+f.read(n)
 
-#for line in f:
-  #print line
+f.readline()
 
-#f = open('output.txt', 'w')
-#f.write('So i see')
-#inscription = ['Rosalind Elsie Franklin ', 1920, 1958]
-#s = str(inscription)
-#f.write(s)
+for line in f:
+  print line
 
-#for i in inscription:
-  #f.write(str(i) + '\n')
-#f.close()
+f = open('output.txt', 'w')
+f.write('So i see')
+inscription = ['Rosalind Elsie Franklin ', 1920, 1958]
+s = str(inscription)
+f.write(s)
 
+for i in inscription:
+  f.write(str(i) + '\n')
+f.close()
 
-d = open('working.txt', 'r')
+# the code for the actual problem is below:
+d = open('working.txt', 'r') # open the file from the computer through code, as opposed to manually inputting data.
 lynuh = 1
 s = open('output.txt','w')
 for line in d:
